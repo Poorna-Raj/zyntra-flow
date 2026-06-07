c30b2210b0ace1a7d322d80cf2e57c426727cf1d
Kusal Adithya <adithyakusal9@gmail.com>
2026-06-07 16:18:03 +0530

    Sql Schema UPDATED!!!!

--- database(supabase)/schema.sql
+++ database(supabase)/schema.sql
@@ -10,12 +10,14 @@ CREATE TABLE public.product (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
   product_name text NOT NULL,
   category text NOT NULL,
+  Price double precision,
+  image_url text,
   CONSTRAINT product_pkey PRIMARY KEY (id)
 );
 CREATE TABLE public.time_dimension (
   id uuid NOT NULL DEFAULT gen_random_uuid(),
   week_number integer,
-  month integer,
+  month text,
   CONSTRAINT time_dimension_pkey PRIMARY KEY (id)
 );
 CREATE TABLE public.season_info (
@@ -44,12 +46,14 @@ CREATE TABLE public.sales_forecast (
   context_id uuid,
   demand_score numeric,
   estimated_units_sold numeric,
+  user_id uuid,
   CONSTRAINT sales_forecast_pkey PRIMARY KEY (id),
   CONSTRAINT sales_forecast_province_id_fkey FOREIGN KEY (province_id) REFERENCES public.province(id),
   CONSTRAINT sales_forecast_product_id_fkey FOREIGN KEY (product_id) REFERENCES public.product(id),
   CONSTRAINT sales_forecast_time_id_fkey FOREIGN KEY (time_id) REFERENCES public.time_dimension(id),
   CONSTRAINT sales_forecast_season_id_fkey FOREIGN KEY (season_id) REFERENCES public.season_info(id),
-  CONSTRAINT sales_forecast_context_id_fkey FOREIGN KEY (context_id) REFERENCES public.context_dimensions(id)
+  CONSTRAINT sales_forecast_context_id_fkey FOREIGN KEY (context_id) REFERENCES public.context_dimensions(id),
+  CONSTRAINT sales_forecast_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.user_profiles(id)
 );
 CREATE TABLE public.user_profiles (
   id uuid NOT NULL,
